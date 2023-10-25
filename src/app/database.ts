928e326afcd804c86006ac6ab3d6536c1b2ee395
Kevin Ingersoll <kingersoll@gmail.com>
2023-10-25 11:52:06 +0100

    fix env var name in error

--- src/app/database.ts
+++ src/app/database.ts
@@ -2,8 +2,8 @@ import * as schema from "./schema";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-// TODO: use zod / parseEnv util
-if (!process.env.DATABASE_URL) throw new Error("Missing DB_URL env variable");
+if (!process.env.DATABASE_URL)
+  throw new Error("Missing DATABASE_URL env variable");
 
 const client = postgres(process.env.DATABASE_URL);
 export const database = drizzle(client, { schema });
