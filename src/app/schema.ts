e2aa61b0bc2c8dbdb0e6f2a28fc5c4610844b4f8
Kevin Ingersoll <kingersoll@gmail.com>
2023-10-25 12:03:18 +0100

    add challenge status schema, fix up other schema to match DB

--- src/app/schema.ts
+++ src/app/schema.ts
@@ -1,15 +1,29 @@
 import { pgTable } from "drizzle-orm/pg-core";
-import { asBigInt, asHex, asAddress } from "@latticexyz/store-sync/postgres";
+import {
+  asBigInt,
+  asHex,
+  asAddress,
+  asNumber,
+} from "@latticexyz/store-sync/postgres";
 
 export const txInputs = pgTable("tx_inputs", {
-  chainId: asBigInt("chain_id", "numeric").notNull(),
-  blockHash: asHex("block_hash").notNull(),
-  blockNumber: asBigInt("block_num", "numeric").notNull(),
-  txHash: asHex("tx_hash").notNull(),
-  txIndex: asBigInt("tx_idx", "numeric").notNull(),
-  txSigner: asAddress("tx_signer").notNull(),
-  txTo: asAddress("tx_to").notNull(),
-  txInput: asHex("tx_input").notNull(),
+  chainId: asNumber("chain_id", "numeric"),
+  blockHash: asHex("block_hash"),
+  blockNumber: asBigInt("block_num", "numeric"),
+  txHash: asHex("tx_hash"),
+  txIndex: asBigInt("tx_idx", "numeric"),
+  txSigner: asAddress("tx_signer"),
+  txTo: asAddress("tx_to"),
+  txInput: asHex("tx_input"),
 });
 
-// TODO: add schema for challenges table
+export const challengeStatus = pgTable("challenge_status", {
+  chainId: asNumber("chain_id", "numeric"),
+  blockHash: asHex("block_hash"),
+  blockNumber: asBigInt("block_num", "numeric"),
+  txHash: asHex("tx_hash"),
+  logAddress: asAddress("log_addr"),
+  challengedHash: asHex("challenged_hash"),
+  challengedBlockNumber: asBigInt("challenged_block_num", "numeric"),
+  status: asNumber("status", "numeric"),
+});
