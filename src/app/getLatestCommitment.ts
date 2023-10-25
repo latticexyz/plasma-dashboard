81f94f71e88e4e4095cde4b0f858b5f3dc7d0289
Kevin Ingersoll <kingersoll@gmail.com>
2023-10-25 16:20:10 +0100

    make batcher vars public

--- src/app/getLatestCommitment.ts
+++ src/app/getLatestCommitment.ts
@@ -4,10 +4,13 @@ import { InputCommitment } from "@/common";
 import { database } from "@/database";
 import { Address } from "viem";
 
-const BATCHER = process.env.BATCHER as Address;
-const BATCHER_INBOX = process.env.BATCHER_INBOX as Address;
-if (!BATCHER || !BATCHER_INBOX)
-  throw new Error("Missing BATCHER or BATCHER_INBOX env variable");
+const NEXT_PUBLIC_BATCHER = process.env.NEXT_PUBLIC_BATCHER as Address;
+const NEXT_PUBLIC_BATCHER_INBOX = process.env
+  .NEXT_PUBLIC_BATCHER_INBOX as Address;
+if (!NEXT_PUBLIC_BATCHER || !NEXT_PUBLIC_BATCHER_INBOX)
+  throw new Error(
+    "Missing NEXT_PUBLIC_BATCHER or NEXT_PUBLIC_BATCHER_INBOX env variable"
+  );
 
 export async function getLatestCommitment(): Promise<
   InputCommitment | undefined
@@ -28,7 +31,10 @@ export async function getLatestCommitment(): Promise<
       },
     },
     where: (table, { and, eq }) =>
-      and(eq(table.txFrom, BATCHER), eq(table.txTo, BATCHER_INBOX)),
+      and(
+        eq(table.txFrom, NEXT_PUBLIC_BATCHER),
+        eq(table.txTo, NEXT_PUBLIC_BATCHER_INBOX)
+      ),
     orderBy: (table, { desc }) => [desc(table.blockNumber)],
   });
   // `commitment.challenge` type is not nullable but should be, see https://github.com/drizzle-team/drizzle-orm/issues/1420
