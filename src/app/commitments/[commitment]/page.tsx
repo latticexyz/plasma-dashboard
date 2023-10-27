a010b7c41615dc70d8455573dd736c4f6ae405c7
Kevin Ingersoll <kingersoll@gmail.com>
2023-10-27 15:49:46 +0100

    add back button

--- src/app/commitments/[commitment]/page.tsx
+++ src/app/commitments/[commitment]/page.tsx
@@ -9,6 +9,7 @@ import { Challenge } from "@/common";
 import { getChallengeConfig } from "@/getChallengeConfig";
 import { getChallengeStatus } from "@/getChallengeStatus";
 import { getCommitment } from "@/getCommitment";
+import { ArrowLeftIcon } from "@/icons/ArrowLeftIcon";
 import { ArrowRightIcon } from "@/icons/ArrowRightIcon";
 import { InboxIcon } from "@/icons/InboxIcon";
 import { SendIcon } from "@/icons/SendIcon";
@@ -46,129 +47,142 @@ export default async function CommitmentPage({ params }: Props) {
   );
 
   return (
-    <div className="divide-y divide-white/20">
-      <div className="flex justify-between gap-8 px-3 py-5">
-        <div className="flex gap-8">
-          <CommitmentBlock commitment={commitment} />
-          <ChallengeStatusCell status={status} />
-        </div>
-        {/* TODO: make these dependent on status */}
-        <div className="flex gap-8">
-          <LabeledCell label="Resolved by" className="text-right">
-            <span className="text-white">
-              {challenge ? (
-                <TruncatedAddress address={challenge.txFrom} />
-              ) : (
-                "??"
-              )}
-            </span>
-          </LabeledCell>
-          <LabeledCell label="Ended" className="text-right">
-            <span className="text-white">00/00 00:00</span>
-          </LabeledCell>
-        </div>
+    <div className="space-y-8">
+      <div className="flex">
+        <Link
+          href="/"
+          className="flex items-center gap-2 px-2 -m-2 font-mono text-sm uppercase transition hover:text-white"
+        >
+          <span className="text-xl">
+            <ArrowLeftIcon />
+          </span>
+          <span>View all blocks</span>
+        </Link>
       </div>
-
-      <div className="flex gap-8 justify-between items-center px-3 py-6">
-        <div className="flex flex-col justify-center gap-3">
-          <div className="flex">
-            <div className="flex flex-shrink-0 w-32 gap-1 items-center">
-              <div className="flex-shrink-0">
-                <SendIcon />
-              </div>
-              <div className="font-mono uppercase text-sm">Sender</div>
-            </div>
-            <div className="font-mono uppercase text-sm text-white">
-              {commitment.txFrom}
-            </div>
+      <div className="divide-y divide-white/20">
+        <div className="flex justify-between gap-8 px-3 py-5">
+          <div className="flex gap-8">
+            <CommitmentBlock commitment={commitment} />
+            <ChallengeStatusCell status={status} />
           </div>
-          <div className="flex">
-            <div className="flex flex-shrink-0 w-32 gap-1 items-center">
-              <div className="flex-shrink-0">
-                <InboxIcon />
-              </div>
-              <div className="font-mono uppercase text-sm">Inbox</div>
-            </div>
-            <div className="font-mono uppercase text-sm text-white">
-              {commitment.txTo}
-            </div>
+          {/* TODO: make these dependent on status */}
+          <div className="flex gap-8">
+            <LabeledCell label="Resolved by" className="text-right">
+              <span className="text-white">
+                {challenge ? (
+                  <TruncatedAddress address={challenge.txFrom} />
+                ) : (
+                  "??"
+                )}
+              </span>
+            </LabeledCell>
+            <LabeledCell label="Ended" className="text-right">
+              <span className="text-white">00/00 00:00</span>
+            </LabeledCell>
           </div>
         </div>
-        <div className="flex-grow flex gap-2 justify-end">
-          <Link
-            // TODO: link to block explorer
-            href={`#${commitment.txHash}`}
-            className="flex-shrink-0 px-4 py-2 bg-white text-black font-mono uppercase text-sm"
-          >
-            View tx
-          </Link>
-          <Link
-            // TODO: link to input data on S3
-            href={`#${commitment.inputHash}`}
-            className="flex-shrink-0 px-4 py-2 bg-white/20 text-white font-mono uppercase text-sm"
-          >
-            Data
-          </Link>
-        </div>
-      </div>
-      <div className="py-3">
-        <table className="w-full">
-          <thead>
-            <tr>
-              <th className="text-left p-3 font-mono uppercase text-xs font-normal">
-                History
-              </th>
-              <th className="text-left p-3 font-mono uppercase text-xs font-normal">
-                Action by
-              </th>
-              <th className="text-left p-3 font-mono uppercase text-xs font-normal">
-                Window
-              </th>
-              <th className="text-left p-3 font-mono uppercase text-xs font-normal">
-                Tx hash
-              </th>
-              <th className="text-left p-3 font-mono uppercase text-xs font-normal"></th>
-            </tr>
-          </thead>
 
-          <tbody>
-            {commitment.challenges.length ? (
-              commitment.challenges.map((challenge) => {
-                const status = getChallengeStatus(
-                  challenge,
-                  challengeConfig,
-                  latestBlockNumber
-                );
-                return (
-                  <tr key={challenge.txHash}>
-                    <td className="p-3 font-mono uppercase text-sm text-white">
-                      <ChallengeStatusIndicator status={status} />
-                    </td>
-                    <td className="p-3 font-mono uppercase text-sm text-white">
-                      <TruncatedAddress address={challenge.txFrom} />
-                    </td>
-                    <td className="p-3 font-mono uppercase text-sm text-white">
-                      TODO window
-                    </td>
-                    <td className="p-3 font-mono uppercase text-sm text-white">
-                      <TruncatedAddress address={challenge.txHash} />
-                    </td>
-                    <td>TODO time ago</td>
-                  </tr>
-                );
-              })
-            ) : (
+        <div className="flex gap-8 justify-between items-center px-3 py-6">
+          <div className="flex flex-col justify-center gap-3">
+            <div className="flex">
+              <div className="flex flex-shrink-0 w-32 gap-1 items-center">
+                <div className="flex-shrink-0">
+                  <SendIcon />
+                </div>
+                <div className="font-mono uppercase text-sm">Sender</div>
+              </div>
+              <div className="font-mono uppercase text-sm text-white">
+                {commitment.txFrom}
+              </div>
+            </div>
+            <div className="flex">
+              <div className="flex flex-shrink-0 w-32 gap-1 items-center">
+                <div className="flex-shrink-0">
+                  <InboxIcon />
+                </div>
+                <div className="font-mono uppercase text-sm">Inbox</div>
+              </div>
+              <div className="font-mono uppercase text-sm text-white">
+                {commitment.txTo}
+              </div>
+            </div>
+          </div>
+          <div className="flex-grow flex gap-2 justify-end">
+            <Link
+              // TODO: link to block explorer
+              href={`#${commitment.txHash}`}
+              className="flex-shrink-0 px-4 py-2 bg-white text-black font-mono uppercase text-sm"
+            >
+              View tx
+            </Link>
+            <Link
+              // TODO: link to input data on S3
+              href={`#${commitment.inputHash}`}
+              className="flex-shrink-0 px-4 py-2 bg-white/20 text-white font-mono uppercase text-sm"
+            >
+              Data
+            </Link>
+          </div>
+        </div>
+        <div className="py-3">
+          <table className="w-full">
+            <thead>
               <tr>
-                <td
-                  colSpan={5}
-                  className="p-3 font-mono uppercase text-sm text-white"
-                >
-                  This input commitment has not been challenged.
-                </td>
+                <th className="text-left p-3 font-mono uppercase text-xs font-normal">
+                  History
+                </th>
+                <th className="text-left p-3 font-mono uppercase text-xs font-normal">
+                  Action by
+                </th>
+                <th className="text-left p-3 font-mono uppercase text-xs font-normal">
+                  Window
+                </th>
+                <th className="text-left p-3 font-mono uppercase text-xs font-normal">
+                  Tx hash
+                </th>
+                <th className="text-left p-3 font-mono uppercase text-xs font-normal"></th>
               </tr>
-            )}
-          </tbody>
-        </table>
+            </thead>
+
+            <tbody>
+              {commitment.challenges.length ? (
+                commitment.challenges.map((challenge) => {
+                  const status = getChallengeStatus(
+                    challenge,
+                    challengeConfig,
+                    latestBlockNumber
+                  );
+                  return (
+                    <tr key={challenge.txHash}>
+                      <td className="p-3 font-mono uppercase text-sm text-white">
+                        <ChallengeStatusIndicator status={status} />
+                      </td>
+                      <td className="p-3 font-mono uppercase text-sm text-white">
+                        <TruncatedAddress address={challenge.txFrom} />
+                      </td>
+                      <td className="p-3 font-mono uppercase text-sm text-white">
+                        TODO window
+                      </td>
+                      <td className="p-3 font-mono uppercase text-sm text-white">
+                        <TruncatedAddress address={challenge.txHash} />
+                      </td>
+                      <td>TODO time ago</td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <tr>
+                  <td
+                    colSpan={5}
+                    className="p-3 font-mono uppercase text-sm text-white"
+                  >
+                    This input commitment has not been challenged.
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
