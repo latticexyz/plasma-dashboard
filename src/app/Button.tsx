4fd61785cdee5620724d78d2b21d1b18913e7b6f
Kevin Ingersoll <kingersoll@gmail.com>
2023-11-03 13:22:18 +0000

    more button style tweaks

--- src/app/Button.tsx
+++ src/app/Button.tsx
@@ -20,9 +20,10 @@ export function Button({
       type={type || "button"}
       aria-busy={pending}
       aria-disabled={disabled}
+      // TODO: disable click handlers during pending/disabled
       className={twMerge(
         "group bg-white text-black transition hover:bg-lime-300",
-        "aria-[busy=true]:hover:bg-amber-200 aria-[busy=true]:cursor-default",
+        "aria-[busy=true]:hover:bg-white aria-[busy=true]:cursor-default",
         "aria-[disabled=true]:bg-neutral-400 aria-[disabled=true]:hover:bg-orange-300 aria-[disabled=true]:cursor-not-allowed",
         className
       )}
@@ -31,19 +32,26 @@ export function Button({
       <span className="inline-grid pointer-events-none overflow-hidden">
         <span
           className={twMerge(
-            "row-start-1 col-start-1 transition opacity-100 flex items-center justify-center",
-            "group-aria-[busy=true]:opacity-10 group-hover:group-aria-[busy=true]:opacity-100"
+            "row-start-1 col-start-1 transition opacity-100 flex items-center justify-center"
           )}
         >
           <span className="px-3 py-2">{children}</span>
         </span>
         <span
           className={twMerge(
-            "row-start-1 col-start-1 transition opacity-0 flex items-center justify-center",
+            "row-start-1 col-start-1 transition opacity-0 bg-gradient-to-r from-transparent via-white/80 to-white",
+            "group-aria-[busy=true]:opacity-100 group-hover:group-aria-[busy=true]:opacity-20"
+          )}
+        ></span>
+        <span
+          className={twMerge(
+            "row-start-1 col-start-1 transition opacity-0 flex items-center justify-end",
             "group-aria-[busy=true]:opacity-100 group-hover:group-aria-[busy=true]:opacity-20"
           )}
         >
-          <PendingIcon />
+          <span className="px-3 py-2">
+            <PendingIcon />
+          </span>
         </span>
       </span>
     </button>
