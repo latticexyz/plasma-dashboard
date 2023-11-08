1235a0c6ca76076c18ef02e31a26eac4cd82ef28
Kevin Ingersoll <kingersoll@gmail.com>
2023-11-08 12:30:12 +0300

    workaround for rainbowkit modal and radix dialog overlapping

--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -1,5 +1,10 @@
 import { ReactNode } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
+import {
+  useAccountModal,
+  useChainModal,
+  useConnectModal,
+} from "@rainbow-me/rainbowkit";
 
 export type Props = {
   open?: boolean;
@@ -9,8 +14,20 @@ export type Props = {
 };
 
 export function Modal({ open, onOpenChange, trigger, children }: Props) {
+  const { connectModalOpen } = useConnectModal();
+  const { accountModalOpen } = useAccountModal();
+  const { chainModalOpen } = useChainModal();
+
+  // workaround for RainbowKit modals overlapping Radix Dialog
+  const isModalDisabled =
+    connectModalOpen || accountModalOpen || chainModalOpen;
+
   return (
-    <Dialog.Root open={open} onOpenChange={onOpenChange}>
+    <Dialog.Root
+      open={open}
+      onOpenChange={onOpenChange}
+      modal={!isModalDisabled}
+    >
       <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-neutral-950/80 animate-in fade-in backdrop-blur grid place-items-center overflow-y-auto">
