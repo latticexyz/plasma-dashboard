b370facfaa3b52c651a59b46f01f191ff2c0da09
Kevin Ingersoll <kingersoll@gmail.com>
2023-11-09 15:15:13 +0300

    remove connect button for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,11 +30,7 @@ export default function Layout({ children }: Props) {
     >
       <body>
         <WalletProvider>
-          {/* TODO: move this to a better spot */}
-          <ConnectButton />
-
           <div className="mx-auto max-w-screen-lg py-16">{children}</div>
-
           <ToastContainer position="bottom-right" theme="dark" />
         </WalletProvider>
       </body>
